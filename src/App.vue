9feaa281bbb048bf9379d41b01c803ce8b490c9a
Marina <bmvmarc@gmail.com>
2021-12-06 19:50:58 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <!--  <menu-bar></menu-bar>-->
+        <div class="app">
+            <h1>APP</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+// import MenuBar from '@/components/Menu';
+
+export default {
+    // components: {
+    //     MenuBar
+    // }
+    
+}
+</script>
+
+
+<style>
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    .app {
+        padding: 20px;
+    }
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+    "mode": "development",
+    entry: {
+      main: "./src/main.js",
+    },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[hash].js"
+    },
+
+    devServer: {
+      port: 3030
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({template: "./src/index.html"}),
+      new CleanWebpackPlugin,
+      new VueLoaderPlugin(),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.(css|less)$/,
+          use: ["style-loader", "css-loader", "less-loader"]
+        },
+        
+        {
+          test: /\.(jpg|jpeg|png|svg)/,
+          use: ["file-loader"]
+        },
+        
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        }
+
+      ]
+    }
+
+  }
